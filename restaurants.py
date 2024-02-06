93ddd6910de9407e9bb11d20578f78126c512fe8
ArnabSahoo <123124773+ArnabSahoo@users.noreply.github.com>
2024-02-06 11:08:43 -0500

    restaurants

---
+++ restaurants.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  6 10:09:04 2024
+
+@author: arnab
+"""
+
+vegetarian = input('Is anyone in your party vegetarian? (Yes or No): ')
+vegan = input('Is anyone in your party vegan? (Yes or No): ')
+gluten_free = input('Is anyone in your party gluten-free? (Yes or No): ')
+
+if vegetarian == 'Yes':
+    if vegan == 'Yes':
+        if gluten_free == 'Yes':
+            print ('\nHere are your resturant choices.\nCorner Café\nThe Chef\'s Kitchen')
+
+if vegetarian == 'Yes':
+    if vegan == 'No':
+        if gluten_free == 'No':
+            print ('\nHere are your resturant choices.\nMama\'s Fine Italian\nMain Street Pizza Company\nCorner Café\nThe Chef\'s Kitchen')
+            
+if vegetarian == 'Yes':
+    if vegan == 'No':
+        if gluten_free == 'Yes':
+            print ('\nHere are your resturant choices.\nMain Street Pizza Company\nCorner Café\nThe Chef\'s Kitchen')
+
+if vegetarian == 'Yes':
+    if vegan == 'Yes':
+        if gluten_free == 'No':
+            print ('\nHere are your resturant choices.\nCorner Café\nThe Chef\'s Kitchen')
+   
+if vegetarian == 'No':
+    if vegan == 'No':
+        if gluten_free == 'No':
+            print ('\nHere are your resturant choices.\nJoe\'s Gourmet Burgers\nMama\'s Fine Italian\nMain Street Pizza Company\nCorner Café\nThe Chef\'s Kitchen')
+            
+if vegetarian == 'No':
+    if vegan == 'No':
+        if gluten_free == 'Yes':
+            print ('\nHere are your resturant choices.\nMain Street Pizza Company\nCorner Café\nThe Chef\'s Kitchen')
+            
+if vegetarian == 'No':
+    if vegan == 'Yes':
+        if gluten_free == 'Yes':
+            print ('\nHere are your resturant choices.\nCorner Café\nThe Chef\'s Kitchen')
+            
+if vegetarian == 'No':
+    if vegan == 'Yes':
+        if gluten_free == 'No':
+            print ('\nHere are your resturant choices.\nCorner Café\nThe Chef\'s Kitchen')
+            
